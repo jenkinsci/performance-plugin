4b3be120bb282c86699f30b7002b355b644e17d7
Anton Goley <antongoley@gmail.com>
2012-06-26 13:21:36 +0300

    New response time graphs selected build and uri report

---
+++ src/main/java/hudson/plugins/performance/JmeterSummarizerParser.java
@@ -0,0 +1,116 @@
+package hudson.plugins.performance;
+
+import hudson.Extension;
+import hudson.util.IOException2;
+import hudson.model.AbstractBuild;
+import hudson.model.TaskListener;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Agoley
+ * Date: 06.02.2012
+ * Time: 12:45:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class JmeterSummarizerParser extends PerformanceReportParser{
+
+    
+    @Extension
+  public static class DescriptorImpl extends PerformanceReportParserDescriptor {
+    @Override
+    public String getDisplayName() {
+      return "JmeterSummarizer";
+    }
+  }
+
+   @DataBoundConstructor
+  public JmeterSummarizerParser(String glob) {
+      super(glob);
+    }
+
+    @Override
+  public String getDefaultGlobPattern() {
+    return "**/*.log";
+  }
+
+   @Override
+   public Collection<PerformanceReport> parse(AbstractBuild<?, ?> build,
+      Collection<File> reports, TaskListener listener)  {
+      List<PerformanceReport> result = new ArrayList<PerformanceReport>();
+
+      PrintStream logger = listener.getLogger();
+      for (File f : reports) {
+         try {
+           final  PerformanceReport r = new PerformanceReport();
+           r.setReportFileName(f.getName());
+           logger.println("Performance: Parsing JMeterSummarizer report file " + f.getName());
+
+           Scanner s = new Scanner(f);
+
+           String key;
+           String line;
+           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd H:m:s"); 
+            while ( s.hasNextLine() )  {
+              line=s.nextLine().replaceAll("="," ");
+
+              if (line.contains ("+"))   {
+              Scanner scanner= new Scanner(line);
+              HttpSample sample = new HttpSample();
+
+              String dateString= scanner.next()+" "+scanner.next();
+
+              sample.setDate(dateFormat.parse(dateString));   
+
+              scanner.findInLine("jmeter.reporters.Summariser:");
+              key=scanner.next();
+              scanner.next();
+              // set SamplesCount
+              scanner.findInLine(key);
+              sample.setSummarizerSamples(scanner.nextLong());
+              // set response time
+              scanner.findInLine("Avg:");
+              sample.setDuration(scanner.nextLong());
+              sample.setSuccessful(true);
+              // set MIN
+              scanner.findInLine("Min:");
+              sample.setSummarizerMin(scanner.nextLong());
+              // set MAX
+              scanner.findInLine("Max:");
+              sample.setSummarizerMax(scanner.nextLong());
+              // set errors count
+              scanner.findInLine("Err:");
+              sample.setSummarizerErrors(scanner.nextInt());
+              //sample.setSummarizerErrors( Float.valueOf(scanner.next().replaceAll("[()%]","")));
+
+              sample.setUri(key);
+              r.addSample(sample);
+             }
+            }
+
+          result.add(r);
+
+         }catch (FileNotFoundException e) {
+          logger.println("File not found" + e.getMessage());
+         }catch (SAXException e) {
+          logger.println(e.getMessage());
+         }catch (ParseException e) {
+
+      }
+      }
+
+    return result;
+
+   }
+
+}
---
+++ src/main/java/hudson/plugins/performance/TrendReportGraphs.java
@@ -0,0 +1,95 @@
+package hudson.plugins.performance;
+
+
+import hudson.model.ModelObject;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+
+public class TrendReportGraphs implements ModelObject
+    {
+
+  private AbstractBuild<?, ?> build;
+  private String filename;
+  private PerformanceReport performanceReport;
+  private AbstractProject<?, ?> project;
+
+
+  public TrendReportGraphs(final AbstractProject<?, ?> project,final AbstractBuild<?, ?> build,
+      final StaplerRequest request, String filename,
+      PerformanceReport performanceReport) {
+    this.build = build;
+    this.filename = filename;
+    this.performanceReport=performanceReport;
+    this.project=project;
+  }
+
+  public void  doRespondingTimeGraph(StaplerRequest request,
+            StaplerResponse response) throws IOException{
+
+    PerformanceReportPosition performanceReportPosition = new PerformanceReportPosition();
+    request.bindParameters(performanceReportPosition);
+
+    PerformanceBuildAction performanceBuildAction = build.getAction(PerformanceBuildAction.class);
+
+      if (performanceBuildAction != null) {
+           if (performanceReport != null) {
+               String uri = performanceReportPosition.getSummarizerTrendUri();
+                   if (uri !=null) {
+                     UriReport uriReport =  performanceReport.getUriReportMap().get(uri);
+                     uriReport.doSummarizerTrendGraph(request, response);
+                   }
+           }
+      }
+  }
+
+        
+  public ArrayList<String> getUris(){
+    ArrayList<String> uriList = new ArrayList<String>();
+    PerformanceReport report = getPerformanceReport();
+
+    if (report != null) {
+        for (String uri:report.getUriReportMap().keySet()) {
+            uriList.add(uri);
+
+        }
+    }
+    return uriList;
+  }
+
+  public UriReport getUriReport(String uri) {
+       if (performanceReport != null) {
+           return performanceReport.getUriReportMap().get(uri);
+       } else {
+           return build.getAction(PerformanceBuildAction.class).getPerformanceReportMap().getUriReport(uri);
+       }
+
+  }
+
+  public String getDisplayName() {
+    return Messages.TrendReportDetail_DisplayName();
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public AbstractProject<?, ?> getProject() {
+    return project;
+  }
+
+  public AbstractBuild<?, ?>  getBuild() {
+    return build;
+  }
+
+  public PerformanceReport  getPerformanceReport() {
+    return performanceReport;
+  }
+
+}
