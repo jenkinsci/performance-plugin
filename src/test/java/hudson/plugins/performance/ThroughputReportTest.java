1110c0be7dfec3f3632b1d8867a8dff346c99c9e
terma <artem.stasuk@gmail.com>
2014-05-27 09:52:02 -0400

    Correct calculate average throughput

--- src/test/java/hudson/plugins/performance/ThroughputReportTest.java
+++ src/test/java/hudson/plugins/performance/ThroughputReportTest.java
@@ -67,4 +67,38 @@ public class ThroughputReportTest {
         Assert.assertEquals(2, throughputReport.getAverage());
     }
 
+    @Test
+    public void shouldReturnAverageThroughput() {
+        long time = System.currentTimeMillis();
+
+        // 0 sec - first request  - 1 sec
+        // 0 sec -                - 1 sec - second request - 2 sec
+        // 0 sec - third request  - 1 sec
+        // 0 sec - four request   - 1 sec
+        // 0 sec - total 3        - 1 sec - total 1        - 2 sec
+        // average 2
+
+        HttpSample httpSample1 = new HttpSample();
+        httpSample1.setDate(new Date(time));
+
+        HttpSample httpSample2 = new HttpSample();
+        httpSample2.setDate(new Date(time + 1000));
+
+        HttpSample httpSample3 = new HttpSample();
+        httpSample3.setDate(new Date(time));
+        httpSample3.setDuration(500);
+
+        HttpSample httpSample4 = new HttpSample();
+        httpSample4.setDate(new Date(time));
+        httpSample4.setDuration(10);
+
+        UriReport uriReport = new UriReport(performanceReport, "f", "x");
+        uriReport.addHttpSample(httpSample1);
+        uriReport.addHttpSample(httpSample2);
+
+        performanceReport.getUriReportMap().put("x", uriReport);
+
+        Assert.assertEquals(2, throughputReport.getAverage());
+    }
+
 }
