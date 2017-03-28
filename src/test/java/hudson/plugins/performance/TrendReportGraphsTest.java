e84000f6c0aa7474ce92db57d6aef7fd2ffc65d4
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-03-28 15:44:52 +0300

    added TrendReportGraphsTest

---
+++ src/test/java/hudson/plugins/performance/TrendReportGraphsTest.java
@@ -0,0 +1,37 @@
+package hudson.plugins.performance;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.performance.reports.PerformanceReport;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.kohsuke.stapler.StaplerRequest;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class TrendReportGraphsTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Mock
+    private StaplerRequest request;
+
+    @Test
+    public void test() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        FreeStyleBuild build = project.createExecutable();
+        PerformanceReport report = new PerformanceReport();
+
+        TrendReportGraphs graphs = new TrendReportGraphs(project, build, request, "simpleFilename", report);
+
+        assertEquals(0, graphs.getUris().size());
+        assertEquals("simpleFilename", graphs.getFilename());
+        assertEquals("Trend report", graphs.getDisplayName());
+        assertEquals(project, graphs.getProject());
+        assertEquals(build, graphs.getBuild());
+        assertEquals(report, graphs.getPerformanceReport());
+        assertEquals(null, graphs.getUriReport("null"));
+    }
+}
