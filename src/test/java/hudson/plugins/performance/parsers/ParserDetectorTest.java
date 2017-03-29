6458a4392bfc480f49d8983da95429d3ef4964a1
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-03-29 17:26:22 +0300

    set file names in test

--- src/test/java/hudson/plugins/performance/parsers/ParserDetectorTest.java
+++ src/test/java/hudson/plugins/performance/parsers/ParserDetectorTest.java
@@ -17,22 +17,22 @@ public class ParserDetectorTest {
         file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
         assertEquals(ParserDetector.ParserType.TAURUS, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/JMeterResults.jtl").toURI());
         assertEquals(ParserDetector.ParserType.JMETER, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/TEST-JUnitResults.xml").toURI());
         assertEquals(ParserDetector.ParserType.JUNIT, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/IagoResults.log").toURI());
         assertEquals(ParserDetector.ParserType.IAGO, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/WrkResultsQuick.wrk").toURI());
         assertEquals(ParserDetector.ParserType.WRK, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/JMeterCsvResults.csv").toURI());
         assertEquals(ParserDetector.ParserType.JMETER_CSV, ParserDetector.detect(file));
 
-        file = new File(getClass().getResource("/TaurusXMLReport.xml").toURI());
+        file = new File(getClass().getResource("/summary.log").toURI());
         assertEquals(ParserDetector.ParserType.JMETER_SUMMARIZER, ParserDetector.detect(file));
     }
 }
