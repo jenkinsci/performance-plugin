12afc03d89f99b64fe5aded57cb2aaab24b7002a
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-03-31 14:47:14 +0300

    add parser factory

---
+++ src/main/java/hudson/plugins/performance/parsers/ParserFactory.java
@@ -0,0 +1,29 @@
+package hudson.plugins.performance.parsers;
+
+import java.io.IOException;
+
+public class ParserFactory {
+
+    public static AbstractParser getParser(String filePath) throws IOException {
+        final String parserName = ParserDetector.detect(filePath);
+
+        if (parserName.equals(JMeterParser.class.getSimpleName())) {
+            return new JMeterParser(filePath);
+        } else if (parserName.equals(JMeterCsvParser.class.getSimpleName())) {
+            return new JMeterCsvParser(filePath);
+        } else if (parserName.equals(JUnitParser.class.getSimpleName())) {
+            return new JUnitParser(filePath);
+        } else if (parserName.equals(TaurusParser.class.getSimpleName())) {
+            return new TaurusParser(filePath);
+        } else if (parserName.equals(WrkSummarizerParser.class.getSimpleName())) {
+            return new WrkSummarizerParser(filePath);
+        } else if (parserName.equals(JmeterSummarizerParser.class.getSimpleName())) {
+            return new JmeterSummarizerParser(filePath, ""); // TODO: summarizer
+        } else if (parserName.equals(IagoParser.class.getSimpleName())) {
+            return new IagoParser(filePath, "", ""); // TODO: IAGO
+        } else {
+            throw new IllegalArgumentException("Unknown parser type: " + parserName);
+        }
+    }
+
+}
