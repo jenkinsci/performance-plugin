f11a60445a49bf79a7d593f63dc580300ac27ac4
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-13 10:55:37 +0300

    added install bzt

--- src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
+++ src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
@@ -7,35 +7,29 @@ import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
-import hudson.model.Computer;
-import hudson.model.Node;
 import hudson.plugins.performance.Messages;
 import hudson.tasks.BatchFile;
 import hudson.tasks.BuildStep;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.tasks.Shell;
-import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
 
 /**
  * "Build step" for running performance test
  */
 public class PerformanceTestBuild extends Builder implements BuildStep {
 
-    protected final static String CHECK_COMMAND = "bzt --help";
+    protected final static String CHECK_BZT_COMMAND = "bzt --help";
+    protected final static String CHECK_VIRTUALENV_COMMAND = "virtual --help";
+    protected final static String CREATE_LOCAL_PYTHON_COMMAND = "virtualenv --clear --system-site-packages taurus-venv";
+    protected final static String VIRTUALENV_PATH = "taurus-venv/bin/";
+    protected final static String INSTALL_BZT_COMMAND = VIRTUALENV_PATH + "pip install bzt";
     protected final static String PERFORMANCE_TEST_COMMAND = "bzt";
     protected final static String DEFAULT_CONFIG_FILE = "defaultReport.yml";
 
@@ -66,19 +60,56 @@ public class PerformanceTestBuild extends Builder implements BuildStep {
 
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-        boolean isBZTInstall = new Shell(CHECK_COMMAND).perform(build, launcher, listener); // TODO: off help output
-        if (!isBZTInstall) {
-            return false;
+        boolean isVirtualenvInstallation = false;
+        // Step 1: Check bzt using "bzt --help".
+        if (!runCommand(CHECK_BZT_COMMAND, build, launcher, listener)) {                                                 // TODO: off help output?
+            // Step 1.1: If bzt not installed check virtualenv using "virtualenv --help".
+            if (runCommand(CHECK_VIRTUALENV_COMMAND, build, launcher, listener)) {                                       // TODO: off help output?
+                // Step 1.2: Create local python using "virtualenv --clear --system-site-packages taurus-venv".
+                if (runCommand(CREATE_LOCAL_PYTHON_COMMAND, build, launcher, listener)) {                                // TODO: off help output?
+                    // Step 1.3: Install bzt in virtualenv using "taurus-venv/bin/pip install bzt".
+                    if (runCommand(INSTALL_BZT_COMMAND, build, launcher, listener)) {                                    // TODO: off help output?
+                        // Step 1.4: Check bzt using "taurus-venv/bin/bzt --help"
+                        if (runCommand(VIRTUALENV_PATH + CHECK_BZT_COMMAND, build, launcher, listener)) {                // TODO: off help output?
+                            isVirtualenvInstallation = true;
+                        } else {
+                            // TODO: what we do when bzt in virtualenv doesn't work?
+                            return false;
+                        }
+                    } else {
+                        // TODO: what we do when bzt doesn't install in virtualenv?
+                        return false;
+                    }
+                } else {
+                    // TODO: what we do when virtualenv does create local python?
+                    return false;
+                }
+            } else {
+                // TODO: what we do when virtualenv does not installed?
+                return false;
+            }
+//            return false;
         }
 
-        String bztExecution = PERFORMANCE_TEST_COMMAND + ' ' +
+        String bztExecution =
+                (isVirtualenvInstallation ? VIRTUALENV_PATH : "") +
+                PERFORMANCE_TEST_COMMAND + ' ' +
                 extractDefaultReportToWorkspace(build.getWorkspace()) + " " +
                 testConfigurationFiles + " " +
                 testOptions;
 
-        Builder builder = Functions.isWindows() ? new BatchFile(bztExecution) : new Shell(bztExecution);
-        return builder.perform(build, launcher, listener);
+        // Step 2: Run performance test.
+        return runCommand(bztExecution, build, launcher, listener);
         // TODO: add post build action
+    }
+
+    public static boolean runCommand(String command, AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)
+            throws IOException, InterruptedException {
+        return createCommand(command).perform(build, launcher, listener);
+    }
+
+    public static Builder createCommand(String command) {
+        return Functions.isWindows() ? new BatchFile(command) : new Shell(command);
     }
 
 
