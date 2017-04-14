7b4fd3a08b2e321d1f6281c4ac9354be4dda3afa
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-14 16:51:36 +0300

    fixed pipline

--- src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
+++ src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
@@ -2,19 +2,14 @@ package hudson.plugins.performance.build;
 
 import hudson.Extension;
 import hudson.FilePath;
-import hudson.Functions;
 import hudson.Launcher;
-import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
 import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.plugins.performance.Messages;
-import hudson.tasks.BatchFile;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
-import hudson.tasks.Shell;
 import jenkins.tasks.SimpleBuildStep;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
@@ -22,7 +17,11 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 /**
@@ -30,12 +29,15 @@ import java.io.PrintStream;
  */
 public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
 
-    protected final static String CHECK_BZT_COMMAND = "bzt --help";
-    protected final static String CHECK_VIRTUALENV_COMMAND = "virtualenv --help";
-    protected final static String CREATE_LOCAL_PYTHON_COMMAND = "virtualenv --clear --system-site-packages taurus-venv";
-    protected final static String VIRTUALENV_PATH = "taurus-venv/bin/";
-    protected final static String INSTALL_BZT_COMMAND = VIRTUALENV_PATH + "pip --no-cache-dir install bzt";
     protected final static String PERFORMANCE_TEST_COMMAND = "bzt";
+    protected final static String VIRTUALENV_COMMAND = "virtualenv";
+    protected final static String HELP_COMMAND = "--help";
+    protected final static String VIRTUALENV_PATH = "taurus-venv/bin/";
+    protected final static String[] CHECK_BZT_COMMAND = new String[]{PERFORMANCE_TEST_COMMAND, HELP_COMMAND};
+    protected final static String[] CHECK_VIRTUALENV_BZT_COMMAND = new String[]{VIRTUALENV_PATH + PERFORMANCE_TEST_COMMAND, HELP_COMMAND};
+    protected final static String[] CHECK_VIRTUALENV_COMMAND = new String[]{VIRTUALENV_COMMAND, HELP_COMMAND};
+    protected final static String[] CREATE_LOCAL_PYTHON_COMMAND = new String[]{VIRTUALENV_COMMAND, "--clear", "--system-site-packages", "taurus-venv"};
+    protected final static String[] INSTALL_BZT_COMMAND = new String[]{VIRTUALENV_PATH + "pip", "--no-cache-dir", "install", PERFORMANCE_TEST_COMMAND};
     protected final static String DEFAULT_CONFIG_FILE = "defaultReport.yml";
 
 
@@ -64,73 +66,111 @@ public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
         this.testOptions = (testOptions == null) ? StringUtils.EMPTY : testOptions;
     }
 
-    @Override
-    public void perform(@Nonnull Run<?, ?> build, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
 
+    @Override
+    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
         PrintStream logger = listener.getLogger();
         boolean isVirtualenvInstallation = false;
         boolean isBztInstalled = false;
 
+
         logger.println("Performance test: Checking bzt installed on your machine.");
         // Step 1: Check bzt using "bzt --help".
-        if (!runCommand(CHECK_BZT_COMMAND, build, launcher, listener)) {                                                 // TODO: off help output?
+        if (!runCmd(CHECK_BZT_COMMAND, workspace, logger, true)) {
             logger.println("Performance test: You have not got bzt on your machine. Next step is checking virtualenv.");
             // Step 1.1: If bzt not installed check virtualenv using "virtualenv --help".
-            if (runCommand(CHECK_VIRTUALENV_COMMAND, build, launcher, listener)) {                                       // TODO: off help output?
+            if (runCmd(CHECK_VIRTUALENV_COMMAND, workspace, logger, true)) {
                 logger.println("Performance test: Checking virtualenv is OK. Next step is creation local python.");
                 // Step 1.2: Create local python using "virtualenv --clear --system-site-packages taurus-venv".
-                if (runCommand(CREATE_LOCAL_PYTHON_COMMAND, build, launcher, listener)) {                                // TODO: off help output?
+                if (runCmd(CREATE_LOCAL_PYTHON_COMMAND, workspace, logger, true)) {
                     logger.println("Performance test: Creation local python is OK. Next step is install bzt.");
                     // Step 1.3: Install bzt in virtualenv using "taurus-venv/bin/pip install bzt".
-                    if (runCommand(INSTALL_BZT_COMMAND, build, launcher, listener)) {                                    // TODO: off help output?
+                    if (runCmd(INSTALL_BZT_COMMAND, workspace, logger, true)) {
                         logger.println("Performance test: bzt installed successfully. Checking bzt.");
                         // Step 1.4: Check bzt using "taurus-venv/bin/bzt --help"
-                        if (runCommand(VIRTUALENV_PATH + CHECK_BZT_COMMAND, build, launcher, listener)) {                // TODO: off help output?
+                        if (runCmd(CHECK_VIRTUALENV_BZT_COMMAND, workspace, logger, true)) {
                             logger.println("Performance test: bzt is working.");
                             isVirtualenvInstallation = true;
-                        } else {
-                            // TODO: what we do when bzt in virtualenv doesn't work?
                         }
-                    } else {
-                        // TODO: what we do when bzt doesn't install in virtualenv?
                     }
-                } else {
-                    // TODO: what we do when virtualenv does create local python?
                 }
-            } else {
-                // TODO: what we do when virtualenv does not installed?
             }
         } else {
+            logger.println("Performance test: bzt is installed on your machine.");
             isBztInstalled = true;
         }
 
         if (isBztInstalled || isVirtualenvInstallation) {
-            String bztExecution =
-                    (isVirtualenvInstallation ? VIRTUALENV_PATH : "") +
-                            PERFORMANCE_TEST_COMMAND + ' ' +
-                            extractDefaultReportToWorkspace(workspace) + " " +
-                            testConfigurationFiles + " " +
-                            testOptions;
 
             // Step 2: Run performance test.
-            if (runCommand(bztExecution, build, launcher, listener)) {
-                build.setResult(Result.SUCCESS);
+            if (runCmd(
+                    new String[]{
+                        (isVirtualenvInstallation ? VIRTUALENV_PATH : "") + PERFORMANCE_TEST_COMMAND,
+                        testConfigurationFiles,
+//                        testOptions,
+                        extractDefaultReportToWorkspace(workspace)
+                    }, workspace, logger, false)) {
+                run.setResult(Result.SUCCESS);
                 return;
             }
         }
 
-        build.setResult(Result.FAILURE);
-
-        // TODO: add post build action
+        run.setResult(Result.FAILURE);
     }
 
-    public static boolean runCommand(String command, Run<?, ?> build, Launcher launcher, TaskListener listener)
-            throws IOException, InterruptedException {
-        return createCommand(command).perform((AbstractBuild<?, ?>) build, launcher, (BuildListener) listener);
+    public static boolean runCmd(String[] commands, FilePath workspace, final PrintStream logger, boolean skipOutput) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        processBuilder.directory(new File(workspace.getRemote()));
+
+        final Process process;
+        try {
+            process = processBuilder.start();
+        } catch (IOException e) {
+            if (!skipOutput) {
+                e.printStackTrace(logger);
+            }
+            return false;
+        }
+
+
+        int code;
+        try {
+            if (!skipOutput) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        printStreamToLogger(process.getInputStream(), logger);
+                    }
+                }.start();
+            }
+            process.getOutputStream().close();
+            code = process.waitFor();
+        } catch (InterruptedException e) {
+            if (!skipOutput) {
+                e.printStackTrace(logger);
+                printStreamToLogger(process.getErrorStream(), logger);
+            }
+            return false;
+        }
+
+
+        if (code != 0 && !skipOutput) {
+            printStreamToLogger(process.getErrorStream(), logger);
+        }
+
+        return code == 0;
     }
 
-    public static Builder createCommand(String command) {
-        return Functions.isWindows() ? new BatchFile(command) : new Shell(command);
+    protected static void printStreamToLogger(InputStream source, PrintStream target) {
+        BufferedReader input = new BufferedReader(new InputStreamReader(source));
+        String line;
+
+        try {
+            while ((line = input.readLine()) != null)
+                target.println(line);
+        } catch (IOException e) {
+            target.println("Reading of error stream caused next exception: " + e.getMessage());
+        }
     }
 
 
