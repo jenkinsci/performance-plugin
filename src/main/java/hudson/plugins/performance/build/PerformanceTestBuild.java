12a193106844dc4157759fe92079d343e51d943d
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-17 14:39:33 +0300

    fixed execution on slave

--- src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
+++ src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
@@ -1,8 +1,10 @@
 package hudson.plugins.performance.build;
 
+import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.Proc;
 import hudson.model.AbstractProject;
 import hudson.model.Result;
 import hudson.model.Run;
@@ -16,15 +18,10 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 import javax.annotation.Nonnull;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -74,27 +71,30 @@ public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
         boolean isVirtualenvInstallation = false;
         boolean isBztInstalled = false;
 
+        EnvVars envVars = run.getEnvironment(listener);
 
         logger.println("Performance test: Checking bzt installed on your machine.");
         // Step 1: Check bzt using "bzt --help".
-        if (!runCmd(CHECK_BZT_COMMAND, workspace, logger, true)) {
-            logger.println("Performance test: You have not got bzt on your machine. Next step is checking virtualenv.");
+        if (!runCmd(CHECK_BZT_COMMAND, workspace, logger, launcher, envVars, true)) {
+            logger.println("Performance test: You have not bzt on your machine. Next step is checking virtualenv.");
             // Step 1.1: If bzt not installed check virtualenv using "virtualenv --help".
-            if (runCmd(CHECK_VIRTUALENV_COMMAND, workspace, logger, true)) {
+            if (runCmd(CHECK_VIRTUALENV_COMMAND, workspace, logger, launcher, envVars, true)) {
                 logger.println("Performance test: Checking virtualenv is OK. Next step is creation local python.");
                 // Step 1.2: Create local python using "virtualenv --clear --system-site-packages taurus-venv".
-                if (runCmd(CREATE_LOCAL_PYTHON_COMMAND, workspace, logger, true)) {
+                if (runCmd(CREATE_LOCAL_PYTHON_COMMAND, workspace, logger, launcher, envVars, true)) {
                     logger.println("Performance test: Creation local python is OK. Next step is install bzt.");
                     // Step 1.3: Install bzt in virtualenv using "taurus-venv/bin/pip install bzt".
-                    if (runCmd(INSTALL_BZT_COMMAND, workspace, logger, true)) {
+                    if (runCmd(INSTALL_BZT_COMMAND, workspace, logger, launcher, envVars, true)) {
                         logger.println("Performance test: bzt installed successfully. Checking bzt.");
                         // Step 1.4: Check bzt using "taurus-venv/bin/bzt --help"
-                        if (runCmd(CHECK_VIRTUALENV_BZT_COMMAND, workspace, logger, true)) {
+                        if (runCmd(CHECK_VIRTUALENV_BZT_COMMAND, workspace, logger, launcher, envVars, true)) {
                             logger.println("Performance test: bzt is working.");
                             isVirtualenvInstallation = true;
                         }
                     }
                 }
+            } else {
+                logger.println("Performance test: You have not virtualenv on your machine. Please, install virtualenv on your machine.");
             }
         } else {
             logger.println("Performance test: bzt is installed on your machine.");
@@ -113,7 +113,7 @@ public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
             }
             testCommand.add(extractDefaultReportToWorkspace(workspace));
             logger.println("Performance test: run " + Arrays.toString(testCommand.toArray()));
-            if (runCmd(testCommand.toArray(new String[testCommand.size()]), workspace, logger, false)) {
+            if (runCmd(testCommand.toArray(new String[testCommand.size()]), workspace, logger, launcher, envVars, false)) {
                 run.setResult(Result.SUCCESS);
                 return;
             }
@@ -122,61 +122,12 @@ public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
         run.setResult(Result.FAILURE);
     }
 
-    public static boolean runCmd(String[] commands, FilePath workspace, final PrintStream logger, boolean skipOutput) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder(commands);
-        processBuilder.directory(new File(workspace.getRemote()));
-
-        final Process process;
-        try {
-            process = processBuilder.start();
-        } catch (IOException e) {
-            if (!skipOutput) {
-                e.printStackTrace(logger);
-            }
-            return false;
-        }
-
-
-        int code;
-        try {
-            if (!skipOutput) {
-                new Thread() {
-                    @Override
-                    public void run() {
-                        printStreamToLogger(process.getInputStream(), logger);
-                    }
-                }.start();
-            }
-            process.getOutputStream().close();
-            code = process.waitFor();
-        } catch (InterruptedException e) {
-            if (!skipOutput) {
-                e.printStackTrace(logger);
-                printStreamToLogger(process.getErrorStream(), logger);
-            }
-            return false;
-        }
-
-
-        if (code != 0 && !skipOutput) {
-            printStreamToLogger(process.getErrorStream(), logger);
-        }
-
-        return code == 0;
+    public static boolean runCmd(String[] commands, FilePath workspace, PrintStream logger, Launcher launcher, EnvVars envVars, boolean skipOutput) throws IOException, InterruptedException {
+        Proc proc = (skipOutput) ?
+                launcher.launch().cmds(commands).envs(envVars).pwd(workspace).start() :
+                launcher.launch().cmds(commands).envs(envVars).stderr(logger).stdout(logger).pwd(workspace).start();
+        return proc.join() == 0;
     }
-
-    protected static void printStreamToLogger(InputStream source, PrintStream target) {
-        BufferedReader input = new BufferedReader(new InputStreamReader(source));
-        String line;
-
-        try {
-            while ((line = input.readLine()) != null)
-                target.println(line);
-        } catch (IOException e) {
-            target.println("Reading of error stream caused next exception: " + e.getMessage());
-        }
-    }
-
 
     protected String extractDefaultReportToWorkspace(FilePath workspace) throws IOException, InterruptedException {
         FilePath defaultConfig = workspace.child(DEFAULT_CONFIG_FILE);
