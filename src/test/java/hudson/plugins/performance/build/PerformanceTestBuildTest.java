eac5b1d1e5b96cb6e2abd7157e7af3fbe84f064f
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-17 17:21:22 +0300

    +test logging

--- src/test/java/hudson/plugins/performance/build/PerformanceTestBuildTest.java
+++ src/test/java/hudson/plugins/performance/build/PerformanceTestBuildTest.java
@@ -10,7 +10,12 @@ import org.junit.Test;
 import org.jvnet.hudson.test.HudsonTestCase;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 
 public class PerformanceTestBuildTest extends HudsonTestCase {
@@ -29,6 +34,31 @@ public class PerformanceTestBuildTest extends HudsonTestCase {
 
         buildTest.perform(buildExt, buildExt.getWorkspace(), createLocalLauncher(), BuildListenerAdapter.wrap(createTaskListener()));
         assertEquals(Result.SUCCESS, buildExt.getResult());
+    }
+
+    @Test
+    public void testLogging() throws Exception {
+
+        PerformanceTestBuild buildTest = new PerformanceTestBuild("--help");
+
+        FreeStyleProject project = createFreeStyleProject();
+        FreeStyleBuildExt buildExt = new FreeStyleBuildExt(project);
+        buildExt.setWorkspace(new FilePath(Files.createTempDir()));
+        buildExt.onStartBuilding();
+        buildExt.getRootDir().mkdirs();
+
+
+        PrintStream out = buildTest.getPerformanceTestLogger(buildExt, System.out);
+        out.println("Test log!");
+        out.close();
+
+        File tmpFile = File.createTempFile("temp.log", "");
+        PrintStream actualLog = new PrintStream(tmpFile);
+        buildTest.printPerformanceTestLog(buildExt, actualLog);
+        actualLog.close();
+
+        String actualLine = new BufferedReader(new FileReader(tmpFile)).readLine();
+        assertEquals("Test log!", actualLine);
     }
 
     public static class FreeStyleBuildExt extends FreeStyleBuild {
