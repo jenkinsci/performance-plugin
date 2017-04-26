6a96083b31ab46d0696c0b898cdacb7683edf715
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-26 16:09:39 +0300

    write only message in console log

--- src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
+++ src/main/java/hudson/plugins/performance/build/PerformanceTestBuild.java
@@ -200,9 +200,8 @@ public class PerformanceTestBuild extends Builder implements SimpleBuildStep {
         try {
             return launcher.launch().cmds(commands).envs(envVars).stdout(logger).stderr(logger).pwd(workspace).start().join() == 0;
         } catch (IOException ex) {
-            // TODO: write only message to output
             // TODO: write stacktrace to jenkins log
-            logger.write(Throwables.getStackTraceAsString(ex).getBytes());
+            logger.write(ex.getMessage().getBytes());
             return false;
         }
     }
