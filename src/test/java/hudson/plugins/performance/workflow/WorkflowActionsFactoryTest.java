4ab754b6b2026898f7611ca45bf857fc5bf64d19
Raphael Pionke <Raphael.Pionke@t-systems.com>
2017-07-12 12:46:30 +0200

    add PerformanceProjectAction only to runs, which contain PerformanceBuildAction (#121)
    * add PerformanceProjectAction only to jobs which use the plugin
    
    * apply canonical POM element order ('mvn tidy:pom') and update workflow dependencies
    
    * fix unit tests

--- src/test/java/hudson/plugins/performance/workflow/WorkflowActionsFactoryTest.java
+++ src/test/java/hudson/plugins/performance/workflow/WorkflowActionsFactoryTest.java
@@ -5,6 +5,7 @@ import hudson.model.Job;
 import hudson.model.Result;
 import hudson.plugins.performance.actions.PerformanceProjectAction;
 import hudson.slaves.DumbSlave;
+import org.apache.commons.io.IOUtils;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
@@ -13,18 +14,19 @@ import org.junit.Test;
 import org.junit.runners.model.Statement;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
 
+import java.net.URL;
 import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class WorkflowActionsFactoryTest {
+    private final WorkflowActionsFactory factory = new WorkflowActionsFactory();
 
     @Rule
     public RestartableJenkinsRule story = new RestartableJenkinsRule();
 
     @Test
     public void testFlow() throws Exception {
-        final WorkflowActionsFactory factory = new WorkflowActionsFactory();
         assertEquals(Job.class, factory.type());
 
         story.addStep(new Statement() {
@@ -34,6 +36,33 @@ public class WorkflowActionsFactoryTest {
                 WorkflowJob p = story.j.createProject(WorkflowJob.class, "demo");
                 p.setDefinition(new CpsFlowDefinition(
                         "node{ echo 'hi, world!' }", true));
+                WorkflowRun r = p.scheduleBuild2(0).waitForStart();
+                story.j.assertBuildStatus(Result.SUCCESS, story.j.waitForCompletion(r));
+                r = p.scheduleBuild2(1).waitForStart();
+                story.j.assertBuildStatus(Result.SUCCESS, story.j.waitForCompletion(r));
+                Collection<? extends Action> actions = factory.createFor(p);
+                assertEquals(0, actions.size());
+            }
+        });
+    }
+
+    @Test
+    public void testFlowWithProjectAction() throws Exception {
+        String fileContents = null;
+
+        URL url = getClass().getResource("/TaurusXMLReport.xml");
+        if (url != null) {
+            fileContents = IOUtils.toString(url);
+        }
+        final String report = fileContents;
+
+        story.addStep(new Statement() {
+            public void evaluate() throws Throwable {
+                DumbSlave s = story.j.createOnlineSlave();
+                s.setLabelString("test performance report DSL function");
+                WorkflowJob p = story.j.createProject(WorkflowJob.class, "demo2");
+                p.setDefinition(new CpsFlowDefinition(
+                        "node{ writeFile file: 'test.xml', text: '''" + report + "'''; perfReport errorFailedThreshold: 2, errorUnstableThreshold: 2, sourceDataFiles: 'test.xml' }", true));
                 WorkflowRun r = p.scheduleBuild2(0).waitForStart();
                 story.j.assertBuildStatus(Result.SUCCESS, story.j.waitForCompletion(r));
                 r = p.scheduleBuild2(1).waitForStart();
