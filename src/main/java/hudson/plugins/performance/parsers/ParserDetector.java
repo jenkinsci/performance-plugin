eda24f1b63fe033901146d06382b0a28724ec192
Till Neunast <till.neunast@chorus.co.nz>
2017-08-31 09:34:08 +1200

    Remove unused import

--- src/main/java/hudson/plugins/performance/parsers/ParserDetector.java
+++ src/main/java/hudson/plugins/performance/parsers/ParserDetector.java
@@ -16,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
